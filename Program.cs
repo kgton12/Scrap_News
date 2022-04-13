14b233e8fe8bed9bb19ff3aad019d7fb99ab82ef
wellington oliveira <wellington.malheiro@gmail.com>
2022-04-13 11:11:50 -0300

    Refatoração e novas funcionalidades

--- Program.cs
+++ Program.cs
@@ -1,25 +1,21 @@
 ﻿using HtmlAgilityPack;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 using System;
-using System.Net;
+using System.Data.SQLite;
 
 namespace MyNamespace
-{
+{    
     class Scrap_News
-    {
+    {       
         static void Main(string[] args)
         {//https://www.youtube.com/watch?v=CA7Hafn3Pow&ab_channel=RaphaelTerciniDev
          //http://www.andrealveslima.com.br/blog/index.php/2016/09/28/trabalhando-com-sqlite-no-c-e-vb-net/
-
-
             /*string path = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
             ChromeOptions options = new ChromeOptions();
             options.AddArgument("user-data-dir-" + path + "ChromeDriver\\Cache");
             ChromeDriver driver = new ChromeDriver(path, options);
             driver.Navigate().GoToUrl("https://olhardigital.com.br/");
             var title = driver.FindElements(By.XPath("//a[@class='card-post type1 img-effect1']"));
-
+            
 
             List<string> list_url = new List<string>();   
 
@@ -30,9 +26,9 @@ namespace MyNamespace
                 Console.WriteLine($"- > " + item.GetAttribute("href"));
             }
 
-            //Insere_url_DB(list_url, "OD");
-            //Procura_news_OD(driver);
-            driver.Quit();*/   
+            Insere_url_DB(list_url, "OD");
+            Procura_news_OD(driver);
+            driver.Quit();*/
 
 
 
@@ -48,87 +44,98 @@ namespace MyNamespace
                 string hrefValue = link.GetAttributeValue("href", string.Empty);
                 list_url.Add(hrefValue);
             }
-
-            //Insere_url_DB(list_url, "OD");
-            Processa_url();
-            
-
-        }
-        public static System.Data.SQLite.SQLiteConnection Conecta_banco()
-        {
-            var CurrentDirectory = Directory.GetCurrentDirectory();
-
-            var conn = new System.Data.SQLite.SQLiteConnection($"Data Source=" + CurrentDirectory + "\\banco.db;");
-            
-            conn.Open();
-            
-            return conn;
+            Insere_url_DB(list_url, "OD");
+            Processa_url();       
         }
         public static void Insere_url_DB(List<string> list, string origem)
         {
-            var conn = Conecta_banco();
-
-            using (var comm = new System.Data.SQLite.SQLiteCommand(conn))
+            using (SQLiteConnection c = new SQLiteConnection(Retorna_sql_conn()))
             {
+                c.Open();
                 foreach (var item in list)
-                {
-                    comm.CommandText = $"INSERT INTO url_news (url,site) VALUES ('{item}','{origem}' )";
-                    comm.ExecuteNonQuery();
-                }                
+                { 
+                    string sql = $"INSERT INTO url_news (url,site) VALUES ('{item}','{origem}' )";
+                    using (SQLiteCommand cmd = new SQLiteCommand(sql, c))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                }
             }
         }
         public static void Processa_url()
         {
-            var conn = Conecta_banco();
-
-            using (var comm = new System.Data.SQLite.SQLiteCommand(conn))
+            using (SQLiteConnection c = new SQLiteConnection(Retorna_sql_conn()))
             {
-                comm.CommandText = $"SELECT * FROM url_news";
-               
-                using (var reader = comm.ExecuteReader())
+                c.Open();
+                string sql = $"SELECT * FROM url_news";
+                List<string> list_url_OD = new List<string>();
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, c))
                 {
-                    while (reader.Read())
+                    using (SQLiteDataReader reader = cmd.ExecuteReader())
                     {
-                        if (reader["site"].ToString() == "OD")
+                        while (reader.Read())
                         {
-                            Carrega_news_OD(reader["url"].ToString());
-                        }                
+                            if (reader["site"].ToString() == "OD")
+                            {
+                                list_url_OD.Add(reader["url"].ToString());
+                                //Carrega_news_OD(reader["url"].ToString());
+                            }
+                        }
+                    }
+                }
+                Carrega_news_OD(list_url_OD);
+            }
+        }
+        public static void Carrega_news_OD(List<string> urls)
+        {
+
+            foreach (var url in urls)
+            {
+                HtmlWeb hw = new HtmlWeb();
+                HtmlDocument doc = new HtmlDocument();
+                doc = hw.Load(url);
+                string titulo = "";
+                string noticia = "";
+
+                HtmlNodeCollection content = doc.DocumentNode.SelectNodes("//*[@id='singleMain']/div[1]/div[3]/div/h1");//*[@id='singleMain']/div[1]/div[1]/div[1]/h1
+
+                foreach (HtmlNode link in content)
+                {
+                    titulo = link.InnerHtml;
+                }
+                //content = doc.DocumentNode.SelectNodes("//*[@id='singleMain']/div[2]/div[1]/div[1]/article/div[1]/p");//*[@id="singleMain"]/div[2]/div[1]/div[1]/article/div[2]
+                
+                content = doc.DocumentNode.SelectNodes(@"//div[@class='post-content']");
+                
+                //estudar para n pegar mais o caminho xpath
+                //$('.post-content').innerText
+                foreach (HtmlNode link in content)
+                {
+                    noticia = $"{noticia} {link.InnerText}";
+                }
+
+                noticia = noticia.Replace("'", "");
+                noticia = noticia.Replace("”", "");
+                noticia = noticia.Replace("\"", "");
+                noticia = noticia.Replace("&nbsp", "");
+
+
+                using (SQLiteConnection c = new SQLiteConnection(Retorna_sql_conn()))
+                {
+                    c.Open();
+                    string sql = $" UPDATE url_news SET    titulo = {titulo},   noticia = {noticia}  WHERE url = '{url}' ";
+                    using (SQLiteCommand cmd = new SQLiteCommand(sql, c))
+                    {
+                        cmd.ExecuteNonQuery();
                     }
                 }
             }
-
         }
-        public static void Carrega_news_OD(string url)
+        public static string Retorna_sql_conn()
         {
-            //titulo
-            //*[@id="singleMain"]/div[1]/div[1]/div[1]/h1
-
-            //noticia
-            //*[@id="singleMain"]/div[1]/div[1]/div[3]/article
-
-            HtmlWeb hw = new HtmlWeb();
-            HtmlDocument doc = new HtmlDocument();
-            doc = hw.Load(url);
-            string titulo = "";
-            string noticia = "";
-
-            HtmlNodeCollection content = doc.DocumentNode.SelectNodes("//*[@id='singleMain']/div[1]/div[1]/div[1]/h1");
-
-            foreach (HtmlNode link in content)
-            {
-                titulo= link.InnerHtml;
-            }
-
-            content = doc.DocumentNode.SelectNodes("//*[@id='singleMain']/div[1]/div[1]/div[3]/article/div[1]/p");
-
-            foreach (HtmlNode link in content)
-            {
-                noticia = $"{noticia} {link.InnerText}";
-            }
-
-            //implementar update. <-------------------
-
-            Console.ReadLine();
+            var CurrentDirectory = Directory.GetCurrentDirectory();
+            string conn = $"Data Source=" + CurrentDirectory + "\\banco.db;";
+            return conn;
         }
     }
 }
